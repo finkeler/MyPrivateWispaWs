40e5d2ad0e00f63b91c8b72d4506f9c1b69995a2
finkeler <finkeler@gmail.com>
2016-04-18 20:34:45 +0300

    merge rest support

--- src/main/java/com/wispa/webserver/web/DomainController.java
+++ src/main/java/com/wispa/webserver/web/DomainController.java
@@ -1,27 +1,60 @@
 package com.wispa.webserver.web;
 
-import org.springframework.stereotype.Controller;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wispa.webserver.model.User;
+import com.wispa.webserver.service.DomainService;
 
 
-@Controller
+
+@RestController
 public class DomainController {
 
-	@RequestMapping("/")
-	@ResponseBody
-	public String helloWorld()
-	{
-		return "My Hello World!";
-		
-
+	@Autowired
+	DomainService domainService;  //Service which will do all data retrieval/manipulation work
+	
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public ResponseEntity<Map<Long, User>> listAllUsers() {
+		Map<Long, User> users = domainService.findAllUsers();
+		if(users.isEmpty()){
+			return new ResponseEntity<Map<Long, User>>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
+		}
+		return new ResponseEntity<Map<Long, User>>(users, HttpStatus.OK);
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/test")
-	@ResponseBody
-	public String helloWorld2()
-	{
-		return "My Hello World 2";
-	}
+    @RequestMapping(method = RequestMethod.POST, value = "/users")
+    ResponseEntity<Void> createUser() {
+        User user = domainService.createUser();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(linkTo(DomainController.class).slash(user.getId()).toUri());
+
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/users/{userId}", produces = { MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<User> getUser(@PathVariable Long userId) {
+    	
+    	User user = domainService.findById(userId);
+    	return new ResponseEntity<User>(user, HttpStatus.OK) ;
+    }
+    
+    @RequestMapping(method = RequestMethod.DELETE, value = "/users/{userId}")
+    ResponseEntity<Void> removeUser(@PathVariable Long userId) {
+    	domainService.deleteUserById(userId);
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+    
 }
